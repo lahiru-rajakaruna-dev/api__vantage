aede027050e7aa71fbb08bc2d77f5fd5e6ab2f26
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-18 09:26:03 +0530

    feat(cache module): assembled

---
+++ src/cache/cache.module.ts
@@ -0,0 +1,21 @@
+import {Module}                             from '@nestjs/common';
+import {ConfigModule}                       from "@nestjs/config";
+import {LoggerModule}                       from "../logger/logger.module";
+import {cacheFactory, TOKEN__CACHE_FACTORY} from './factory.cache/factory.cache';
+import {NodeCacheService}                   from './node.cache/node.cache.service';
+
+
+
+@Module({
+			imports  : [
+				LoggerModule,
+				ConfigModule
+			],
+			providers: [
+				NodeCacheService,
+				cacheFactory
+			],
+			exports  : [TOKEN__CACHE_FACTORY]
+		})
+export class CacheModule {
+}

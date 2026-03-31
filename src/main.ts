29ea6dcab57958f8fa5eb732577b54b2906cb46b
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-03-31 14:29:48 +0530

    style(main.ts): formatting changes

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,61 @@
-import { NestFactory } from '@nestjs/core';
-import cookieParser from 'cookie-parser';
-import * as z from 'zod';
-import { AppModule } from './app.module';
-import { EDatabaseStrategy, EEnvVars, ENodeEnv, EOrmStrategy } from './types';
+import {NestFactory}                                         from '@nestjs/core';
+import cookieParser                                          from 'cookie-parser';
+import * as z                                                from 'zod';
+import {AppModule}                                           from './app.module';
+import {EDatabaseStrategy, EEnvVars, ENodeEnv, EOrmStrategy} from './types';
+
+/*
+
+process.on("warning", (...args) => console.log(args))
+process.on("beforeExit", (...args) => {
+	console.log(args)
+})
+
+*/
 
 async function bootstrap() {
-  checkEnv();
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      credentials: true,
-      preflightContinue: false,
-      origin: true,
-      optionsSuccessStatus: 200,
-    },
-  });
-  app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+	checkEnv();
+	const app = await NestFactory.create(AppModule, {
+		cors: {
+			credentials         : true,
+			preflightContinue   : false,
+			origin              : true,
+			optionsSuccessStatus: 200,
+		},
+	});
+	app.use(cookieParser());
+	await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
 
 async function checkEnv() {
-  const config = process.env;
+	const config = process.env;
 
-  const drizzle_postgres = z.object({
-    [EEnvVars.NODE_ENV]: z.enum([ENodeEnv.PRODUCTION, ENodeEnv.TEST]),
-    [EEnvVars.PADDLE_PRODUCTION_API_KEY]: z.string().nonempty().nonoptional(),
-    [EEnvVars.ORM_STRATEGY]: z.enum(EOrmStrategy),
-    [EEnvVars.DATABASE_STRATEGY]: z.enum(EDatabaseStrategy),
-    [EEnvVars.POSTGRES_URL]: z.string().nonempty().nonoptional(),
-    [EEnvVars.POSTGRES_USERNAME]: z.string().nonempty().nonoptional(),
-    [EEnvVars.POSTGRES_PASSWORD]: z.string().nonempty().nonoptional(),
-    [EEnvVars.JWT_SECRET_KEY]: z.string().nonempty().nonoptional(),
-  });
+	const drizzle_postgres = z.object({
+										  [EEnvVars.NODE_ENV]                 : z.enum([
+																						   ENodeEnv.PRODUCTION,
+																						   ENodeEnv.TEST
+																					   ]),
+										  [EEnvVars.PADDLE_PRODUCTION_API_KEY]: z.string().nonempty().nonoptional(),
+										  [EEnvVars.ORM_STRATEGY]             : z.enum(EOrmStrategy),
+										  [EEnvVars.DATABASE_STRATEGY]        : z.enum(EDatabaseStrategy),
+										  [EEnvVars.POSTGRES_URL]             : z.string().nonempty().nonoptional(),
+										  [EEnvVars.POSTGRES_USERNAME]        : z.string().nonempty().nonoptional(),
+										  [EEnvVars.POSTGRES_PASSWORD]        : z.string().nonempty().nonoptional(),
+										  [EEnvVars.JWT_SECRET_KEY]           : z.string().nonempty().nonoptional(),
+									  });
 
-  const drizzle_sqlite = z.object({
-    [EEnvVars.NODE_ENV]: z.enum([ENodeEnv.DEVELOPMENT]),
-    [EEnvVars.PADDLE_DEVELOPMENT_API_KEY]: z.string().nonempty().nonoptional(),
-    [EEnvVars.ORM_STRATEGY]: z.enum(EOrmStrategy),
-    [EEnvVars.DATABASE_STRATEGY]: z.enum(EDatabaseStrategy),
-    [EEnvVars.SQLITE_URL]: z.string().nonempty().nonoptional(),
-    [EEnvVars.JWT_SECRET_KEY]: z.string().nonempty().nonoptional(),
-  });
+	const drizzle_sqlite = z.object({
+										[EEnvVars.NODE_ENV]                  : z.enum([ENodeEnv.DEVELOPMENT]),
+										[EEnvVars.PADDLE_DEVELOPMENT_API_KEY]: z.string().nonempty().nonoptional(),
+										[EEnvVars.ORM_STRATEGY]              : z.enum(EOrmStrategy),
+										[EEnvVars.DATABASE_STRATEGY]         : z.enum(EDatabaseStrategy),
+										[EEnvVars.SQLITE_URL]                : z.string().nonempty().nonoptional(),
+										[EEnvVars.JWT_SECRET_KEY]            : z.string().nonempty().nonoptional(),
+									});
 
-  const applicationConfigStrategy = z.xor([drizzle_sqlite, drizzle_postgres]);
+	const applicationConfigStrategy = z.xor([drizzle_sqlite, drizzle_postgres]);
 
-  applicationConfigStrategy.parse(config);
+	applicationConfigStrategy.parse(config);
 }
